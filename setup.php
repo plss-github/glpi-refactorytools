fae77e69e58fc2d836815205be5a4d93983cd177
Higor Freitas <HigorFreitaz@users.noreply.github.com>
2026-09-26 13:34:10 -0300

    bump

--- setup.php
+++ setup.php
@@ -33,7 +33,7 @@ use GlpiPlugin\Refactorytools\ProfileRights;
 use GlpiPlugin\Refactorytools\ReservationMenu;
 use GlpiPlugin\Refactorytools\Share;
 
-define('PLUGIN_REFACTORYTOOLS_VERSION', '0.11.9');
+define('PLUGIN_REFACTORYTOOLS_VERSION', '0.11.10');
 
 // Alvo: GLPI 11.0.x. As assinaturas usadas aqui (Planning::$rightname,
 // CFG_GLPI['planning_types'], populatePlanning(), Html::requireJs('fullcalendar'))
